b87b5a41cc9d4a3d2920c352b50e044e3ecbc009
Stan <s2000ProMax@gmail.com>
2022-03-24 00:08:40 +0300

    typescript 09-type guard

--- 09-type_guard/app.ts
+++ 09-type_guard/app.ts
@@ -45,3 +45,59 @@ const setRoleZero = (user: User | Admin) => {
         throw new Error('User is not Admin');
     }
 }
+
+//Task
+interface IPayment {
+    sum: number;
+    from: number;
+    to: number;
+}
+
+enum PaymentStatus {
+    Success = 'success',
+    Failed = 'failed',
+}
+
+interface IPaymentRequest extends IPayment {
+}
+
+interface IDataSuccess extends IPayment {
+    databaseId: number;
+}
+
+interface IDataFailed {
+    errorMessage: string;
+    errorCode: number;
+}
+
+interface IResponseSuccess {
+    status: PaymentStatus.Success;
+    data: IDataSuccess;
+}
+
+interface IResponseFailed {
+    status: PaymentStatus.Failed;
+    data: IDataFailed;
+}
+
+
+type f = (res: IResponseSuccess | IResponseFailed) => number;
+type Res = IResponseSuccess | IResponseFailed;
+
+const isSuccess = (res: Res): res is IResponseSuccess => {
+    if (res.status === PaymentStatus.Success) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const getIdFromData = (res: Res): number => {
+    if (isSuccess(res)) {
+        return res.data.databaseId;
+    } else {
+        throw new Error(res.data.errorMessage);
+    }
+}
+
+
