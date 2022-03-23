156c03ee99e41e6f6ea15dd460932819503d9fc1
Stan <s2000ProMax@gmail.com>
2022-03-23 15:11:45 +0300

    typescript 03-interfaces task

--- 03-interfaces/app.ts
+++ 03-interfaces/app.ts
@@ -66,3 +66,73 @@ const t = user.password?.type;
 const test = (param?: string) => {
     const t = param ?? multiply(5)
 }
+
+//Task
+enum PaymentStatus {
+    Success = 'success',
+    Failed = 'failed',
+}
+interface IPayment {
+    sum: number,
+    from: number,
+    to: number
+}
+
+interface IPaymentRequest extends IPayment { }
+
+interface IResponseDataSuccess extends IPayment{
+    databaseId: number;
+}
+
+interface IResponseDataFailed {
+    errorMessage: string;
+    errorCode: number;
+
+}
+/*
+interface IResponce {
+    status: PaymentStatus;
+    data: IResponseDataSuccess | IResponseDataFailed;
+}
+
+ */
+
+interface IResponceSuccess {
+    status: PaymentStatus.Success;
+    data: IResponseDataSuccess;
+}
+
+interface IResponceFailed {
+    status: PaymentStatus.Failed;
+    data: IResponseDataFailed;
+}
+
+// const get = (): IResponceSuccess | IResponceFailed => { }
+
+// Запрос в виде платежа
+/*
+{
+    "sum": 10000,
+    "from": 2,
+    "to": 4
+}
+*/
+// Ответ
+/*
+{
+    "status": "success",
+    "data": {
+    "databaseId": 567,
+        "sum": 10000,
+        "from": 2,
+        "to": 4
+}
+},
+{
+    "status": "failed",
+    "data": {
+    "errorMessage": "Недостаточно средств",
+        "errorCode": 4
+}
+}
+*/
